f1080b14a685e973e0b792b1ed8c12857a6a8798
andresbonito <andresbonito@users.noreply.github.com>
2021-03-30 14:55:28 -0300

    Aula 7 incompleta

---
+++ Aula 7/src/bonito/andre/Usuario.java
@@ -0,0 +1,14 @@
+package bonito.andre;
+//o ponto principal disso seria para simplificar
+//características em comum entre classes
+
+public class Usuario {
+    private String nome;
+    private int id;
+
+    public Usuario(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+
+    }
+}
